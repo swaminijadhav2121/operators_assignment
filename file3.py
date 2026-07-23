027e0abdae102bc227952d36bc82ae529b72dff3
swaminijadhav2121 <swaminijadhav1121@gmail.com>
2026-07-23 18:13:40 +0530

    using assignment operators

---
+++ file3.py
@@ -0,0 +1,24 @@
+x = 10
+
+print("Initial value:", x)
+
+x += 5
+print("After += :", x)
+
+x -= 3
+print("After -= :", x)
+
+x *= 2
+print("After *= :", x)
+
+x /= 4
+print("After /= :", x)
+
+x //= 2
+print("After //= :", x)
+
+x %= 3
+print("After %= :", x)
+
+x **= 2
+print("After **= :", x)

a4e6dcc736b0b6b5a004091ed61b96d88a9bbb1d
swaminijadhav2121 <swaminijadhav1121@gmail.com>
2026-07-23 18:32:25 +0530

    using assignment operators

---
+++ file4.py
@@ -0,0 +1,31 @@
+x=15
+print (x)
+
+y=10
+y+=7
+print (y)
+
+z=18
+z-=5
+print (z)
+
+p=25
+p*=5
+print(p)
+
+q=20
+q/=2
+print (q)
+
+r=15
+r%=5
+print (r)
+
+s=30
+s//=2
+
+t=20
+t**=2
+print (t)
+
+
